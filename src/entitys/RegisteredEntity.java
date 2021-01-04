2b69e2fc81903df6257388fa353dbd4df412446e
Pegacraffft <karl.w.schramm@gmail.com>
2021-01-04 08:14:37 +0100

    added relevant entities

---
+++ src/entitys/RegisteredEntity.java
@@ -0,0 +1,20 @@
+package entitys;
+
+import engine.Entity;
+
+public class RegisteredEntity extends Entity {
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void logicLoop() {
+
+    }
+
+    @Override
+    public void renderLoop() {
+
+    }
+}
