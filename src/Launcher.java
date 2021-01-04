244487726bc7073dc6c959699e46779123486861
Pegacraffft <karl.w.schramm@gmail.com>
2021-01-04 08:09:03 +0100

    created scene and window

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,6 +1,19 @@
+import engine.Display;
+import engine.Game;
+import scenes.MainScene;
+
+import java.awt.*;
+
 public class Launcher {
 
     public static void main(String[] args){
+        Display d = Game.display("main");
 
+        Game.addScene(new MainScene(), "main");
+        d.attachScene("main");
+
+        d.setBackgroundColor(Color.darkGray);
+
+        Game.start();
     }
 }
---
+++ src/scenes/MainScene.java
@@ -0,0 +1,20 @@
+package scenes;
+
+import engine.Scene;
+
+public class MainScene extends Scene {
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void logicLoop() {
+
+    }
+
+    @Override
+    public void renderLoop() {
+
+    }
+}
